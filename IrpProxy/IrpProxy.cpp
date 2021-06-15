e685aedfa9c76137d8a59d2fd97d59281a7bfe09
RichardA <richard.abouchaaya@student-cs.fr>
2021-06-16 00:59:23 +0200

    Defining basic driver routines

--- IrpProxy/IrpProxy.cpp
+++ IrpProxy/IrpProxy.cpp
@@ -1,4 +1,8 @@
 #include <ntddk.h>
+#include "common.h"
+#include "IrpProxy.h"
+
+PDRIVER_OBJECT targetDriver;
 
 void IrpProxyUnload(PDRIVER_OBJECT);
 NTSTATUS IrpProxyCreateClose(PDEVICE_OBJECT, PIRP);
@@ -12,5 +16,108 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) 
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = IrpProxyCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_CLOSE] = IrpProxyCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = IrpProxyDeviceControl;
+
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\IrpProxy");
+
+	PDEVICE_OBJECT DeviceObject;
+	NTSTATUS status = IoCreateDevice(
+		DriverObject,
+		0,
+		&devName,
+		FILE_DEVICE_UNKNOWN,
+		0,
+		FALSE,
+		&DeviceObject
+	);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create device object (0x%08X)\n", status));
+		return status;
+	}
+
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\IrpProxy");
+	status = IoCreateSymbolicLink(&symLink, &devName);
+	if(!NT_SUCCESS(status)){
+		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
+		IoDeleteDevice(DeviceObject);
+		return status;
+	}
+
+	return STATUS_SUCCESS;
+}
+
+void IrpProxyUnload(PDRIVER_OBJECT DriverObject) {
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\IrpProxy");
+	IoDeleteSymbolicLink(&symLink);
+
+	IoDeleteDevice(DriverObject->DeviceObject);
+}
+
+NTSTATUS IrpProxyCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
+}
+
+
+NTSTATUS IrpProxyDeviceControl(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+	auto stack = IoGetCurrentIrpStackLocation(Irp);
+	auto status = STATUS_SUCCESS;
+
+	switch (stack->Parameters.DeviceIoControl.IoControlCode) {
+	case IOCTL_SELECT_TARGET: {
+		/* Receive a driver name as an input buffer. 
+		Hook the corresponding driver. */
+
+		if (targetDriver) {
+			KdPrint(("Target driver already selected!\n"));
+			status = STATUS_INVALID_PARAMETER;
+		}
+		if (stack->Parameters.DeviceIoControl.InputBufferLength <= 0) {
+			status = STATUS_BUFFER_TOO_SMALL;
+			break;
+		}
+
+		auto driverName = (PUNICODE_STRING)stack->Parameters.DeviceIoControl.Type3InputBuffer;
+
+		if (driverName == nullptr) {
+			status = STATUS_INVALID_PARAMETER;
+			break;
+		}
+
+		KdPrint(("Attempting to hook : %wZ\n", driverName));
+
+		status = ObReferenceObjectByName(driverName, OBJ_CASE_INSENSITIVE,
+			nullptr, 0, *IoDriverObjectType, KernelMode,
+			nullptr, (PVOID*)&targetDriver);
+
+
+
+
+		break;
+	}
+	default: {
+		status = STATUS_INVALID_DEVICE_REQUEST;
+		break;
+	}
+	}
+
+	Irp->IoStatus.Status = status;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return status;
+}
+
+/* This function will replace the target driver's handlers when hooking */
+NTSTATUS DispatchProxyHandler(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+
+	UNREFERENCED_PARAMETER(Irp);
+	UNREFERENCED_PARAMETER(DeviceObject);
+	return STATUS_SUCCESS;
 }
 
